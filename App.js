dd85eedad4db0ea03ed658ac952cf2afa6829e53
es2 <es2js2@gmail.com>
2019-02-14 05:21:04 +0900

    test

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Dimensions, Platform, TextInput } from 'react-native';
+
+const { height, width } = Dimensions.get('window');
 
 export default class App extends React.Component {
+  state = {
+    NewToDo: '',
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Kawai To Do</Text>
+        <View style={styles.card}>
+          <TextInput 
+            style={styles.input} 
+            placeholder={'New To Do'} 
+            value={newTodo} 
+            onChangeText={this._constrolNewToDo}
+            placeholderTextColor={'#999'}
+            returnKeyType={'done'}
+            autoCorrect={flase}
+            />
+        </View>
       </View>
     );
+  };
+  _constrolNewToDo = text => {
+    this.setState({
+      newTodo: text
+    });
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f23657',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',    
   },
+  title: {
+    color: 'white',
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: '100',
+    marginBottom: 30,
+  },
+  card: {
+    backgroundColor: 'white',
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,    
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgb(50, 50, 50)',
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0,
+        },
+      },
+      android: {        
+        elevation: 30,
+      },
+    })    
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 25,
+  },  
 });
