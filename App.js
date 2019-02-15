adfdfb36647b58757f4ad350ffa9349f699b5cf5
es2 <es2js2@gmail.com>
2019-02-15 14:20:47 +0900

    리액트 네이티브로 투두앱 만들기 #12

--- App.js
+++ App.js
@@ -1,31 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar, Dimensions, Platform, TextInput, ScrollView } from 'react-native';
-import ToDo from './ToDo';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Dimensions,
+  Platform,
+  TextInput,
+  ScrollView
+} from "react-native";
+import { AppLoading } from "expo";
+import uuidv1 from "uuid/v1";
+import ToDo from "./ToDo";
 
-const { height, width } = Dimensions.get('window');
+const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   state = {
-    newToDo: '',
+    newToDo: "",
+    loadedToDos: false,
+    toDos: {}
   };
+
+  componentDidMount = () => {
+    this._loadToDos();
+  };
+
   render() {
+    const { newToDo, loadedToDos, toDos } = this.state;
+    console.log(toDos);
+
+    if (!loadedToDos) {
+      return <AppLoading />;
+    }
+
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}>Kawai To Do</Text>
         <View style={styles.card}>
-          <TextInput 
-            style={styles.input} 
-            placeholder={'New To Do'} 
-            value={this.newToDo} // 또는 const { newToDo } = this.state; 로 따로 선언해도 됨.
+          <TextInput
+            style={styles.input}
+            placeholder={"New To Do"}
+            value={newToDo} // 또는 const { newToDo } = this.state; 로 따로 선언해도 됨.
             onChangeText={this._constrolNewToDo}
-            placeholderTextColor={'#999'}
-            returnKeyType={'done'}
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"}
             autoCorrect={false}
-            />
-            <ScrollView contentContainerStyle={styles.toDos}> 
-              <ToDo />
-            </ScrollView>
+            onSubmitEditing={this._addToDo}
+          />
+          <ScrollView contentContainerStyle={styles.toDos}>
+            {/* <ToDo text={"Hello"} /> */}
+            {Object.values(toDos).map(toDo => (
+              <ToDo
+                key={toDo.id}
+                {...toDo}
+                deleteToDo={this._deleteToDo}
+                uncompleteToDo={this._uncompleteToDo}
+                completeToDo={this._completeToDo}
+                {...toDo}
+              />
+            ))}
+          </ScrollView>
         </View>
       </View>
     );
@@ -35,51 +71,155 @@ export default class App extends React.Component {
     this.setState({
       newToDo: text
     });
-  }
+  };
+
+  _loadToDos = () => {
+    this.setState({
+      loadedToDos: true
+    });
+  };
+
+  _addToDo = () => {
+    const { newToDo } = this.state;
+
+    if (newToDo !== "") {
+      this.setState({
+        newToDo: ""
+      });
+
+      this.setState(prevState => {
+        const ID = uuidv1();
+
+        const newToDoObject = {
+          [ID]: {
+            id: ID,
+            isCompleted: false,
+            text: newToDo,
+            createAt: Date.now()
+          }
+        };
+
+        const newState = {
+          ...prevState,
+          newToDo: "",
+          toDos: {
+            ...prevState.toDos,
+            ...newToDoObject
+          }
+        };
+
+        return { ...newState };
+      });
+      // const toDos = {
+      //   1: {
+      //     id: 1,
+      //     text: "배터리",
+      //     isCompleted: false,
+      //     date: 312415234
+      //   },
+      //   2: {
+      //     id: 2,
+      //     text: "배터리",
+      //     isCompleted: false,
+      //     date: 312415234
+      //   },
+      //   3: {
+      //     id: 3,
+      //     text: "배터리",
+      //     isCompleted: false,
+      //     date: 312415234
+      //   }
+      // };
+    }
+  };
+
+  _deleteToDo = id => {
+    this.setState(prevState => {
+      const toDos = prevState.toDos;
+      delete toDos[id];
+      const newState = {
+        ...prevState,
+        ...toDos
+      };
+      return { ...newState };
+    });
+  };
+
+  _uncompleteToDo = id => {
+    this.setState(prevState => {
+      const newState = {
+        ...prevState,
+        toDos: {
+          ...prevState.toDos,
+          [id]: {
+            ...prevState.toDos[id],
+            isCompleted: false
+          }
+        }
+      };
+      return { ...newState };
+    });
+  };
+
+  _completeToDo = id => {
+    this.setState(prevState => {
+      const newState = {
+        ...prevState,
+        toDos: {
+          ...prevState.toDos,
+          [id]: {
+            ...prevState.toDos[id],
+            isCompleted: true
+          }
+        }
+      };
+      return { ...newState };
+    });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f23657',
-    alignItems: 'center',
-    // justifyContent: 'center',    
+    backgroundColor: "#f23657",
+    alignItems: "center"
+    // justifyContent: 'center',
   },
   title: {
-    color: 'white',
+    color: "white",
     fontSize: 30,
     marginTop: 50,
-    fontWeight: '100',
-    marginBottom: 30,
+    fontWeight: "100",
+    marginBottom: 30
   },
   card: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     flex: 1,
     width: width - 25,
     borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,    
+    borderTopRightRadius: 10,
     ...Platform.select({
       ios: {
-        shadowColor: 'rgb(50, 50, 50)',
+        shadowColor: "rgb(50, 50, 50)",
         shadowOpacity: 0.5,
         shadowRadius: 5,
         shadowOffset: {
           height: -1,
-          width: 0,
-        },
+          width: 0
+        }
       },
-      android: {        
-        elevation: 30,
-      },
-    })    
+      android: {
+        elevation: 30
+      }
+    })
   },
   input: {
     padding: 20,
-    borderBottomColor: '#bbb',
+    borderBottomColor: "#bbb",
     borderBottomWidth: 1,
-    fontSize: 25,
+    fontSize: 25
   },
   toDos: {
-    alignItems: 'center',
+    alignItems: "center"
   }
 });
--- ToDo.js
+++ ToDo.js
@@ -1,72 +1,187 @@
-import React, { Component } from 'react';
-import { 
-  View, 
-  Text, 
-  TouchableOpacity, 
+import React, { Component } from "react";
+import {
+  View,
+  Text,
   StyleSheet,
   Dimensions,
-} from 'react-native';
+  TouchableOpacity,
+  TextInput
+} from "react-native";
+import PropTypes from "prop-types";
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
+
 export default class ToDo extends Component {
-  state = {
-    isEditing: false,
-    isCompleted: false,
-  };
-  render(){
-    const { isCompleted } = this.state;
-    return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={this._toggleComplete}>
-        <View style={[
-          styles.circle, 
-          isCompleted ? styles.completedCircle : styles.uncompletedCircle
-          ]} />
-      </TouchableOpacity>
-      <Text style={[styles.text, isCompleted ? styles.completedText : styles.uncompletedText]}>안녕하세요!!!</Text>
-    </View>
-    );
-  };
-  _toggleComplete = () => {
-    this.setState(prevState => {
-      return {
-        isCompleted: !prevState.isCompleted
-      }
-    })
+  constructor(props) {
+    super(props);
+    this.state = { isEditing: false, toDoValue: props.text };
   }
+
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    isCompleted: PropTypes.bool.isRequired,
+    id: PropTypes.string.isRequired,
+    deleteToDo: PropTypes.func.isRequired,
+    uncompleteToDo: PropTypes.func.isRequired,
+    completeToDo: PropTypes.func.isRequired
+  };
+
+  render() {
+    const { isEditing, toDoValue } = this.state;
+    const { isCompleted, text, id, deleteToDo } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.column}>
+          {/* 동그라미 버튼 */}
+          <TouchableOpacity onPress={this._toggleComplete}>
+            <View
+              style={[
+                styles.circle,
+                isCompleted ? styles.completedCircle : styles.uncompletedCircle
+              ]}
+            />
+          </TouchableOpacity>
+
+          {/* 인풋 박스 */}
+          {isEditing ? (
+            <TextInput
+              style={[
+                styles.input,
+                styles.text,
+                isCompleted ? styles.completedText : styles.uncompletedText
+              ]}
+              value={toDoValue}
+              multiline={true}
+              onChangeText={this._controlInput}
+              returnKeyType={"done"}
+              onBlur={this._finishEditing}
+            />
+          ) : (
+            <Text
+              style={[
+                styles.text,
+                isCompleted ? styles.completedText : styles.uncompletedText
+              ]}
+            >
+              {text}
+            </Text>
+          )}
+        </View>
+
+        {isEditing ? (
+          <View style={styles.actions}>
+            <TouchableOpacity onPressOut={this._finishEditing}>
+              <View style={styles.actionsContainer}>
+                <Text style={styles.actionText}>&#x2714;</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View style={styles.actions}>
+            {/* 수정 */}
+            <TouchableOpacity onPressOut={this._startEditing}>
+              <View style={styles.actionContainer}>
+                <Text style={styles.actionText}>&#x270F;</Text>
+              </View>
+            </TouchableOpacity>
+            {/* 삭제 */}
+            <TouchableOpacity onPressOut={() => deleteToDo(id)}>
+              <View style={styles.actionContainer}>
+                <Text style={styles.actionText}>&#x274C;</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
+    );
+  }
+
+  _toggleComplete = () => {
+    const { isCompleted, uncompleteToDo, completeToDo, id } = this.props;
+    if (isCompleted) {
+      uncompleteToDo(id);
+    } else {
+      completeToDo(id);
+    }
+    // this.setState(prevState => {
+    //   return {
+    //     isCompleted: !prevState.isCompleted
+    //   };
+    // });
+  };
+
+  _startEditing = () => {
+    // const { text } = this.props;
+    this.setState({
+      isEditing: true
+      // toDoValue: text
+    });
+  };
+
+  _finishEditing = () => {
+    this.setState({
+      isEditing: false
+    });
+  };
+
+  // 입력창의 값이 변하면 toDo의 값을 입력된 text로 바꾼다.
+  _controlInput = text => {
+    this.setState({
+      toDoValue: text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     width: width - 50,
-    borderBottomColor: '#bbb',
+    borderBottomColor: "#bbb",
     borderBottomWidth: StyleSheet.hairlineWidth,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
   },
   circle: {
     width: 30,
     height: 30,
     borderRadius: 15,
-    borderColor: 'red',
     borderWidth: 3,
-    marginRight: 20,
+    marginRight: 20
   },
   completedCircle: {
-    borderColor: '#bbb',
-  }, 
+    borderColor: "#bbb"
+  },
   uncompletedCircle: {
-    borderColor: '#f23657',
-  }, 
+    borderColor: "#f23657"
+  },
   text: {
-    fontWeight: '600',
+    fontWeight: "600",
     fontSize: 20,
-    marginVertical: 20,
+    marginVertical: 20
   },
   completedText: {
-    color: '#bbb',    
+    color: "#bbb",
+    textDecorationLine: "line-through"
   },
-  // uncompletedText: {
-
-  // }
+  uncompletedText: {
+    color: "#353839"
+  },
+  column: {
+    flexDirection: "row",
+    alignItems: "center"
+    // justifyContent: 'space-between',
+    // width: width / 2,
+  },
+  actions: {
+    flexDirection: "row"
+  },
+  actionContainer: {
+    marginVertical: 15,
+    marginHorizontal: 13
+  },
+  input: {
+    marginVertical: 15,
+    width: width / 2
+  }
 });
